d308bde6621eb9e3e39d496baa8ab81753370188
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2025-01-17 14:35:20 +0800

    更新 mandu.js

--- mandu.js
+++ mandu.js
@@ -2,9 +2,9 @@
 
 项目名称：漫读-电子书阅读器
 下载地址：https://t.cn/A6TeeJiV
-更新日期：2024-04-30
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
+更新日期：2025-01-17
+脚本作者：@ddm1026
+电报频道：https://t.me/ddm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
@@ -12,14 +12,7 @@
 [rewrite_local]
 ^http:\/\/enjoy_reading_pro\.particlethink\.com:8083\/app\/api\/v\d\/user\/is_vip url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/mandu.js
 
-[mitm]
-hostname = pro.particlethink.com
-
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
-
-chxm1023.data = true;
-
-$done({body : JSON.stringify(chxm1023)});
+var ddm = JSON.parse($response.body);ddm.data = true;$done({body : JSON.stringify(ddm)});
