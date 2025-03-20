21285ac24648de17ca8eed5d48ddb61008c6ca2b
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2025-03-20 14:53:17 +0800

    更新 Tripsy.js

--- Tripsy.js
+++ Tripsy.js
@@ -2,9 +2,9 @@
 
 项目名称：Tripsy-旅行规划者
 下载地址：https://t.cn/A60jMYum
-更新日期：2024-02-22
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
+更新日期：2025-03-20
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
@@ -18,16 +18,15 @@ hostname = firstclass.tripsy.app
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
-
-chxm1023 = {
+let ddm = {
   "is_premium" : true,
-  "expiration_intent" : 1,
+  "expiration_intent" : 0,
   "is_trial" : true,
-  "subscription" : "premium_plan",
-  "expiration_date" : "2099-09-09T09:09:09Z",
+  "subscription" : "premium.lifetime",
+  "is_renew_active" : true,
+  "expiration_date" : null,
   "is_introductory_offer" : true,
-  "is_in_billing_retry_period" : true
+  "is_in_billing_retry_period" : false
 };
 
-$done({body : JSON.stringify(chxm1023)});
+$done({body : JSON.stringify(ddm)});
