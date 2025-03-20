0e1482fde734b22c3f1bef2dec4824c4f635203a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2025-03-21 00:43:44 +0800

    更新 Wall.js

--- Wall.js
+++ Wall.js
@@ -2,15 +2,16 @@
 
 项目名称：Wallcraft-动态壁纸
 下载地址：http://t.cn/A6iO7Eht
-更新日期：2024-03-01
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
+更新日期：2025-03-21
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
 [rewrite_local]
-^https?:\/\/billing-ios\.wallpaperscraft\.com\/(verify_receipt|products)\/remove_ads$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Wall.js
+^https?:\/\/billing-ios\.wallpaperscraft\.com\/v\d\/(verify_receipt|products)\/remove_ads url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Wall.js
+
 
 [mitm]
 hostname = billing-ios.wallpaperscraft.com
@@ -18,24 +19,31 @@ hostname = billing-ios.wallpaperscraft.com
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
-const vipa = '/verify_receipt/remove_ads';
-const vipb = '/products/remove_ads';
+let ddm = JSON.parse($response.body), id = "com.wallpaperscraft.wallpapers.year.1.5baks";
 
-if ($request.url.indexOf(vipa) != -1){
-  chxm1023.items["all_time"] = {
+if(/verify_receipt\/remove_ads/.test($request.url)){
+  ddm.items["all_time"] = {
     "type" : "nonconsumable",
     "is_active" : true
   };
-  chxm1023.items["com.wallpaperscraft.wallpapers.year.1.5baks"] = {
+  ddm.items[id] = {
     "type" : "subscription",
     "already_used_introductory_price" : false,
     "is_active" : true
   };
 }
 
-if ($request.url.indexOf(vipb) != -1){
-  chxm1023 = {  "items" : {   "nonconsumables" : [  "all_time" ],   "subscriptions" : [  "com.wallpaperscraft.wallpapers.year.1.5baks" ]}};
+if(/products\/remove_ads/.test($request.url)){
+  ddm = {
+    "items" : {
+      "nonconsumables" : [
+        "all_time"
+      ],
+      "subscriptions" : [
+        id
+      ]
+    }
+  };
 }
 
-$done({body : JSON.stringify(chxm1023)});
+$done({body : JSON.stringify(ddm)});
