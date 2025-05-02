fd7379c8954a601fa9792cfb0b64948523a91e8f
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2025-05-02 16:05:27 +0800

    更新 iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -1,7 +1,7 @@
 /*************************************
 
 项目名称：iTunes-系列解锁合集
-更新日期：2025-04-26
+更新日期：2025-05-02
 脚本作者：@ddm1023
 电报频道：https://t.me/ddm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -29,6 +29,7 @@ const yearlysubscription = `${bundle_id}.yearlysubscription`;
 const lifetimeid = `${bundle_id}.lifetime`;
 
 const list = {
+  'MediaConvert': { cm: 'timeb', hx: 'hxpda', id: "MediaConverter_ProVersion" },  //格式转换
   'FDSunAlly': { cm: 'timeb', hx: 'hxpda', id: "com.freenotes.sunally.lifetime" },  //SunAlly-智能健康助力
   'Period': { cm: 'timeb', hx: 'hxpda', id: "com.hanchongzan.time.pro" },  //时光提醒
   'com.sixiaobo.MusCut': { cm: 'timeb', hx: 'hxpdb', id: "com.purecollage.pro" },  //无损拼图
