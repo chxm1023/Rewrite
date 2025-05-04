48233b88113683fd5da7e70d6f8df76dc3de9998
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2025-05-04 16:00:43 +0800

    更新 Reheji.js

--- Reheji.js
+++ Reheji.js
@@ -51,7 +51,7 @@ const bundle = {
 };
 
 const listua = {
-  'Dailyart': { name: 'lifeTime', id: 'artLifeTime', cm: 'sjc' },  //Dailyart
+  'Dailyart': { name: 'lifeTime', id: 'artLifeTime', cm: 'sjc' },  //Dailyart-每日艺术
   'Lightune': { name: 'pro', id: 'Lightune_Pro_Year', cm: 'sja' },  //Lightune - AI专业修图
   'ArchiveList': { name: 'pro_life', id: 'com.jy.ArchiveBox.pro_1', cm: 'sjb' },  //ArchiveList - 收藏夹/稍后阅读
   'smscat': { name: 'pro', id: 'smscat_vip_lifetime', cm: 'sjb' },  //短信喵
