2b03b1a514f662af35be39e29fa1c270ef533b14
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2025-05-18 17:31:44 +0800

    创建 znqdq.js

---
+++ znqdq.js
@@ -0,0 +1,27 @@
+/*************************************
+
+应用名称：智能抢答器
+脚本功能：解锁会员
+下载地址：https://t.cn/A6g6Zm0G
+更新日期：2025-05-18
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/qdq\.9sm\.net\/api\/userInfo\/getUserInfo url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/znqdq.js
+
+[mitm]
+hostname = qdq.9sm.net
+
+*************************************/
+
+
+var ddm = JSON.parse($response.body);
+
+ddm.data.expiredTime = "2099-09-09 09:09:09";
+ddm.data.vipType = 1;
+
+$done({body : JSON.stringify(ddm)});
