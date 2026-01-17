e03defd00cdfe260e02570b43c017f1ef5b098ce
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-01-17 15:02:11 +0800

    创建 jzwnl.js

---
+++ jzwnl.js
@@ -0,0 +1,29 @@
+/*************************************
+
+应用名称：吉真万年历
+脚本功能：解锁VIP
+下载地址：https://too.st/fSC
+更新日期：2026-01-17
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/calendarsrv\.iwzwh\.com\/api\/.+\/user\/getvipinfo url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/jzwnl.js
+
+[mitm]
+hostname = calendarsrv.iwzwh.com
+
+*************************************/
+
+
+var ddm = JSON.parse($response.body);
+
+ddm.data = {
+  "expires" : "2099-09-09 09:09:09",
+  "vipLevel" : 1
+};
+
+$done({body : JSON.stringify(ddm)});
