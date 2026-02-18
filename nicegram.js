5e08516057a320d54902ac24151650a83f464cdd
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-02-18 13:37:48 +0800

    更新 nicegram.js

--- nicegram.js
+++ nicegram.js
@@ -12,7 +12,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/(nicegram\.cloud\/api\/v\d\/(user\/info|unblock-feature\/get-settings)|restore-access\.indream\.app\/restoreAccess|api\.mbrx\.app\/v\d\/purchase\/ios\/transaction) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Nicegram.js
+^https?:\/\/(nicegram\.cloud\/api\/v\d\/(user\/info|unblock-feature\/get-settings)|restore-access\.indream\.app\/restoreAccess|api\.mbrx\.app\/v\d\/purchase\/ios\/transaction) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/nicegram.js
 
 [mitm]
 hostname = nicegram.cloud, restore-access.indream.app, api.mbrx.app
