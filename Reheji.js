2522b79fedc6149f4c10a452a42af66906f6b7ce
Tony Wang <37318682+w366813640@users.noreply.github.com>
2026-03-10 13:36:59 +0800

    111

--- Reheji.js
+++ Reheji.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/w366813640/Rewrite/refs/heads/main/Reheji.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/w366813640/Rewrite/refs/heads/main/Reheji.js
 
 [mitm]
 hostname = api.revenuecat.com, api.rc-backup.com
