757f7f6b020b984a9f5f8fd6c1a6b9a5f0ecbc42
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-05-02 10:35:48 +0800

    更新 RawPix.js

--- RawPix.js
+++ RawPix.js
@@ -11,7 +11,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.rawpixlive\.com\/api\/sign\/profile url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/RawPix.js
+^https?:\/\/api\.rawpixlive\.com\/api\/(sign\/profile|picture\/checkout|config\/config) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/RawPix.js
 
 [mitm]
 hostname = api.rawpixlive.com
@@ -21,11 +21,54 @@ hostname = api.rawpixlive.com
 
 var ddm = JSON.parse($response.body);
 
-ddm.data.isCreator = 1;
-ddm.data.is_vip = true;
-ddm.data.expireTime = 4092599349;
-ddm.data.level = 6;
-ddm.data.level_name = "年卡会员";
-ddm.data.can_trail = false;
+if(/api\/sign\/profile/.test($request.url)){
+  ddm.data.isCreator = 1;
+  ddm.data.is_vip = true;
+  ddm.data.expireTime = 4092599349;
+  ddm.data.level = 6;
+  ddm.data.level_name = "年卡会员";
+  ddm.data.can_trail = false;
+}
+
+if(/api\/picture\/checkout/.test($request.url)){
+  ddm = {
+  "msg" : "ok",
+    "data" : {
+      "can_ad_times" : 9999,
+      "message" : "VIP无限次使用",
+      "config" : {
+        "isOpenSingle" : true,
+        "isVipWhenSingle" : false,
+        "isOpenAd" : false,
+        "isOpenVip" : true
+      },
+      "action" : 2
+    },
+    "code" : 0
+  };
+}
+
+if(/api\/config\/config/.test($request.url)){
+  ddm.data.screenAdId = "";
+  ddm.data.ad1 = "";
+  ddm.data.ad2 = "";
+  ddm.data.ad3 = "";
+  ddm.data.ad4 = "";
+  ddm.data.ad5 = "";
+  ddm.data.interAdImage = "";
+  ddm.data.interstitialAdTime = 0;
+  ddm.data.intervalAd = 0;
+  ddm.data.background = "";
+  ddm.data.backgroundImg = "";
+  ddm.data.enterImg = "";
+  ddm.data.enterEwm = "";
+  ddm.data.waterText = "";
+  ddm.data.waterImage = "";
+  ddm.data.waterOpacity = 0;
+  ddm.data.isOpenAd = true;
+  ddm.data.isOpenSingle = true;
+  ddm.data.isVipWhenSingle = true;
+  ddm.data.isOpenVip = true;
+}
 
 $done({ body: JSON.stringify(ddm) });
