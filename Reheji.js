b6fb05c35df79d5d995a13e8a36052d602b58dc7
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-06-19 15:27:16 +0800

    更新 Reheji.js

--- Reheji.js
+++ Reheji.js
@@ -1,7 +1,7 @@
 /*************************************
 
 项目名称：Revenuecat系列解锁合集
-更新日期：2026-06-18
+更新日期：2026-06-19
 脚本作者：@ddm1023
 电报频道：https://t.me/ddm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -56,6 +56,7 @@ const bundle = {
 };
 
 const listua = {
+  'Ticket%20Lover': { name: 'com.minardwu.ticketlover_lifetime', id: 'com.minardwu.ticketlover_lifetime', cm: 'sjb' },  //票恋-TicketLover
   'powo': { name: 'com.minardwu.powo_lifetime', id: 'com.minardwu.powo_lifetime', cm: 'sjb' },  //Powo-桌面照片墙
   'rxframe': { name: 'com.minardwu.rxframe_lifetime', id: 'com.minardwu.rxframe_lifetime', cm: 'sjb' },  //Rxframe-照片边框水印
   'MemeLoop': { name: 'pro', id: 'com.ai.gif.lifetime.pro', cm: 'sjb' },  //GIFMaker,GIFEditor
