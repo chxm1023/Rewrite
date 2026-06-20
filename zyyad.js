98c8aacd42a324109c695e435052b48404119966
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-06-20 15:25:40 +0800

    创建 zyyad.js

---
+++ zyyad.js
@@ -0,0 +1,22 @@
+/*************************************
+
+应用名称：赵云与阿斗-小游戏
+脚本功能：免广告领奖励
+下载地址：https://shm.to/zyyad
+更新日期：2026-06-20
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/api.*\.mihuangame\.com\/api\/v\d\/sys\/user\/login url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/zyyad.js
+
+[mitm]
+hostname = api*.mihuangame.com
+
+*************************************/
+
+
+var ddm = JSON.parse($response.body);ddm.data.userType = 1;$done({ body: JSON.stringify(ddm) });
