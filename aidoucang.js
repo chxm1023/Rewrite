c4013f10fba5a9d7945110769b74a2d595241fb9
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-08-10 16:21:55 +0800

    创建 aidoucang.js

---
+++ aidoucang.js
@@ -0,0 +1,64 @@
+/*************************************
+
+应用名称：AI豆仓
+脚本功能：高级会员
+下载地址：https://apps.apple.com/app/id6760929988
+更新日期：2026-08-10
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/api\.aidoucang\.cn\/api\/me url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/aidoucang.js
+
+[mitm]
+hostname = api.aidoucang.cn
+
+*************************************/
+
+
+var ddm = JSON.parse($response.body);
+
+Object.assign(ddm.data, {
+  "isVip": true,
+  "subscriptionType": "yearly",
+  "formalSubscriptionType": "yearly",
+  "membershipSource": "formal",
+  "subscriptionExpiresAt": "2099-09-09T09:09:09.000Z",
+  "formalSubscriptionExpiresAt": "2099-09-09T09:09:09.000Z"
+});
+
+// VIP AI 配额
+if (ddm.data.aiQuota) {
+  Object.assign(ddm.data.aiQuota, {
+    "isVip": true,
+    "remaining": ddm.data.aiQuota.limit
+  });
+}
+
+// VIP 功能开关
+if (ddm.data.featureFlags) {
+  Object.assign(ddm.data.featureFlags, {
+    "highlight": true,
+    "pixelPattern": true,
+    "forceCloudHighlight": true,
+    "beadBuild": true,
+  });
+}
+
+// 放开已有功能权限，保留服务端原有结构
+if (ddm.data.featureAccessMap) {
+  Object.keys(ddm.data.featureAccessMap).forEach(function(key) {
+    var feature = ddm.data.featureAccessMap[key];
+
+    if (feature && typeof feature === "object") {
+      Object.assign(feature, {
+        "allowed": true,
+      });
+    }
+  });
+}
+
+$done({ body: JSON.stringify(ddm)});
