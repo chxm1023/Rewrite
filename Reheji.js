4b2f091f5d11efa55401e8f98efbb51a305b098b
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-08-11 18:47:53 +0800

    更新 Reheji.js

--- Reheji.js
+++ Reheji.js
@@ -1,7 +1,7 @@
 /*************************************
 
 项目名称：Revenuecat系列解锁合集
-更新日期：2026-08-08
+更新日期：2026-08-11
 脚本作者：@ddm1023
 电报频道：https://t.me/ddm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -63,6 +63,7 @@ const bundle = {
 };
 
 const listua = {
+  'iOS/3001101': { name: 'doneitPremium', id: 'DesignTech.SIA.Doneit.Premium.Plan.Lifetime', cm: 'sjc' },  //看板-Doneit
   'Speedometer': { name: 'SpeedometerLite Pro', id: 'speedometerlite_lifetime', cm: 'sjb' },  //Speedometer-速度计简易版
   'demo_FingerChooser': { name: 'pro', id: 'chooser.pro.lifetime', cm: 'sjb' },  //OneChoose-随机抽签工具
   'scanbatch': { name: 'Pro', id: 'scanbatch.lifetime', cm: 'sjc' },  //码立得•条码二维码批量神器-ScanBatch
