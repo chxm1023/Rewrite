bfb684ab736a81a5d365f2c92a5f12fb2b273d8f
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-09-02 18:06:41 +0800

    更新 paibanrili.js

--- paibanrili.js
+++ paibanrili.js
@@ -1,15 +1,17 @@
 /*************************************
 
-项目名称：排班日历
+应用名称：排班日历
+脚本功能：VIP会员
 下载地址：https://t.cn/A6Nz3Zuo
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
+更新日期：2026-09-02
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
 [rewrite_local]
-^https?:\/\/schedule-api\.julanling\.com\/api\/(get_member_info|vip_detail) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/paibanrili.js
+^https?:\/\/schedule-api\.julanling\.com\/.+\/(get_member_info|vip_detail|get_splash) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/paibanrili.js
 
 [mitm]
 hostname = schedule-api.julanling.com
@@ -17,55 +19,64 @@ hostname = schedule-api.julanling.com
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
-const vipa = '/api/get_member_info';
-const vipb = '/api/vip_detail';
+var ddm = JSON.parse($response.body);
+var url = $request.url;
+var expireTime = "2099-09-09 09:09:09";
+var startTime = "2026-06-06 06:06:06";
 
-if ($request.url.indexOf(vipa) != -1){
-  chxm1023.results.grade = 3;
-  chxm1023.results.upgrade_grade_img = "https://cdn.julanling.com/Fqnty5qaZwXwHkIXj5jtpX4FvBS5";
-  chxm1023.results.benefits = {
-      "data_backup" : {
-        "expire_time" : "2099-09-09 09:09:09",
-        "start_time" : "2023-04-03 09:39:23",
-        "business_type" : "data_backup",
-        "attribute" : [
-
-        ]
-      },
-      "more_shift" : {
-        "expire_time" : "2099-09-09 09:09:09",
-        "start_time" : "2023-04-03 09:39:23",
-        "business_type" : "more_shift",
-        "attribute" : [
-          {
-            "LIMIT" : "100"
-          }
-        ]
-      },
-      "vip_identity" : {
-        "expire_time" : "2099-09-09 09:09:09",
-        "start_time" : "2023-04-03 09:39:23",
-        "business_type" : "vip_identity",
-        "attribute" : [
-
-        ]
-      },
-      "android_widget_month" : {
-        "expire_time" : "2099-09-09 09:09:09",
-        "start_time" : "2023-04-03 09:39:23",
-        "business_type" : "android_widget_month",
-        "attribute" : [
-
-        ]
+var benefits = {
+  "data_backup": {
+    "expire_time": expireTime,
+    "start_time": startTime,
+    "business_type": "data_backup",
+    "attribute": []
+  },
+  "more_shift": {
+    "expire_time": expireTime,
+    "start_time": startTime,
+    "business_type": "more_shift",
+    "attribute": [
+      {
+        "LIMIT": "100"
       }
-   };
+    ]
+  },
+  "vip_identity": {
+    "expire_time": expireTime,
+    "start_time": startTime,
+    "business_type": "vip_identity",
+    "attribute": []
+  },
+  "android_widget_month": {
+    "expire_time": expireTime,
+    "start_time": startTime,
+    "business_type": "android_widget_month",
+    "attribute": []
+  }
+};
+
+if (!ddm.results || typeof ddm.results !== "object") {
+  ddm.results = {};
 }
 
-if ($request.url.indexOf(vipb) != -1){
-  chxm1023.results.card_number = "666666";
-  chxm1023.results.grade = 3;
+if (url.includes("get_member_info")) {
+  Object.assign(ddm.results, {
+    "grade": 3,
+    "upgrade_grade_img": "",
+    "benefits": benefits
+  });
+
 }
 
+else if (url.includes("vip_detail")) {
+  Object.assign(ddm.results, {
+    "card_number": "ddm1023",
+    "grade": 3
+  });
+}
 
-$done({body : JSON.stringify(chxm1023)});
+else if (url.includes("get_splash")) {
+  ddm.results = [];
+}
+
+$done({ body: JSON.stringify(ddm)});
