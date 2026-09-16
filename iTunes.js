d0f322da491a344dfc283ca8dd20d937d32ca62b
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2026-09-16 18:16:09 +0800

    更新 iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -1,7 +1,7 @@
 /*************************************
 
 项目名称：iTunes-系列解锁合集
-更新日期：2026-09-15
+更新日期：2026-09-16
 脚本作者：@ddm1023
 电报频道：https://t.me/ddm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -344,7 +344,7 @@ const autoMap = {
     'co.vulcanlabs'  //Vulcan Labs Company Limited
   ],
   forever: [
-    
+    'com.paha.CapyMood'  //CapyMood-轻松水豚HRV自测
   ]
 };
 
